2123330721110d7b33c35353c8d898f7aaf71170
Ganji Venu <ganjivenu143@gmail.com>
2026-05-23 09:33:03 +0530

    fix: manual CORS headers

--- server/app.js
+++ server/app.js
@@ -6,10 +6,14 @@ const connectDB = require("./src/config/db");
 const app = express();
 connectDB();
 
-app.use(cors({ 
-  origin: [process.env.CLIENT_URL, "http://localhost:5173"],
-  credentials: true 
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
