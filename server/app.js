d9c37da9148ae1273cd5b771f8c4ac4317a1dd3a
Ganji Venu <ganjivenu143@gmail.com>
2026-05-23 13:07:40 +0530

    fix: replace manual CORS middleware with cors package

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
@@ -6,13 +7,12 @@ const connectDB = require("./src/config/db");
 const app = express();
 connectDB();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
+app.use(cors({
+  origin: "https://tripcraft-nu.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
