caad4be5d68c8045d95183f2fd28ec92013c6fa7
Ganji Venu <ganjivenu143@gmail.com>
2026-05-23 09:19:17 +0530

    fix: correct render URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tripcraft-server.onrender.com/api",
+  baseURL: "https://tripcraft-server-gjgn.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
